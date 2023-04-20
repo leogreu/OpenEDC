ca32f7903893b10046213b2f3e59e185c0507bb6
Leonard Greulich <leonard.greulich@gmail.com>
2023-04-20 11:37:58 +0800

    feat: add Korean localization

--- js/metadatamodule.js
+++ js/metadatamodule.js
@@ -975,7 +975,7 @@ function setIOListeners() {
     let inputElements = $$("#details-panel input, #details-panel textarea, #details-panel select, #details-panel #translation-textarea-formatted");
     for (const inputElement of inputElements) {
         inputElement.oninput = () => highlightSaveButton();
-        inputElement.onkeydown = event => {
+        inputElement.onkeypress = event => {
             if (event.code == "Escape") {
                 event.preventDefault();
                 document.activeElement.blur();
